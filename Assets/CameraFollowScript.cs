a5421d2edb89d46a16654ae43eec16a2dcbe2578
monkeyhammer42 <michael-whiddett@hotmail.com>
2021-01-29 23:07:21 +0000

    Added cat and scene

---
+++ Assets/CameraFollowScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+
+    public Transform _player;
+    [SerializeField]
+    private int xCameraOffset;
+    [SerializeField]
+    private int yCameraOffset;
+    [SerializeField]
+    private int zCameraOffset;
+    [SerializeField]
+    private int xPositionCameraOffset;
+    [SerializeField]
+    private int yPositionCameraOffset;
+    [SerializeField]
+    private int zPositionCameraOffset;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtPlayer();
+        FollowPlayer();
+    }
+    void LookAtPlayer()
+    {
+        this.gameObject.transform.LookAt(new Vector3(_player.position.x + xCameraOffset, _player.position.y+yCameraOffset, _player.position.z+zCameraOffset));
+    }
+    void FollowPlayer()
+    {
+        this.gameObject.transform.position=new Vector3(_player.position.x + xPositionCameraOffset, _player.position.y + yPositionCameraOffset, _player.position.z + zPositionCameraOffset);
+
+    }
+}
